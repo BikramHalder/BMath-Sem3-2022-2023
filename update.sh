0f9398ca10df9742c966b62ed8466800cc5f7200
Bikram Halder <bikram0x@outlook.com>
2022-10-16 18:31:26 +0000

    Change minor config

--- update.sh
+++ update.sh
@@ -4,10 +4,10 @@ git pull
 
 if [ -n "$(git status --porcelain)" ]; then
   IFS=''
-  read -p "Commit Message: " commit_msg
+  read -rp "Commit Message: " commit_msg
   git add --all
-  git commit -m $commit_msg
+  git commit -m "$commit_msg"
   git push
 else
-  echo "Nothing to be pushed!";
+  echo "Nothing to be pushed!"
 fi
